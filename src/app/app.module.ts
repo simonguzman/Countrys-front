512c93eaa7c280cfe53fab7f6d86ddf22ae0617d
simonguzman <siguzman@unicauca.edu.co>
2025-01-17 20:11:19 -0500

    Initial proyect commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    SharedModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1'
+
+  constructor(private http: HttpClient) { }
+
+  searchCountryByAlphaCode(code:string): Observable<Country | null> {
+    const url = ` ${this.apiUrl}/alpha/${ code } `
+    return this.http.get<Country[] >(url)
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null),
+        catchError(() => of(null))
+      );
+  }
+
+
+  searchCapital( term: string ): Observable<Country[ ]>{
+    const url = ` ${this.apiUrl}/capital/${ term } `
+    return this.http.get<Country[] >(url)
+      .pipe(
+        catchError(() => of([]))
+      );
+  }
+
+  searchCountry( term: string ): Observable<Country[ ]>{
+    const url = ` ${this.apiUrl}/name/${ term } `
+    return this.http.get<Country[] >(url)
+      .pipe(
+        catchError(() => of([]))
+      );
+  }
+
+  searchRegion( term: string ): Observable<Country[ ]>{
+    const url = ` ${this.apiUrl}/region/${ term } `
+    return this.http.get<Country[] >(url)
+      .pipe(
+        catchError(() => of([]))
+      );
+  }
+}
